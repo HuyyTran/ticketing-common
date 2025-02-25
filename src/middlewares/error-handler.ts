5420d8bb03dad79aa80b759a247270dec797fe19
HuyyTran <huytrn.dev@gmail.com>
2025-02-26 00:17:40 +0700

    prettier

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -5,7 +5,7 @@ export const errorHandler = (
 	err: Error,
 	req: Request,
 	res: Response,
-	next: NextFunction
+	next: NextFunction,
 ): void => {
 	if (err instanceof CustomError) {
 		res.status(err.statusCode).send({ errors: err.serializeErrors() });
